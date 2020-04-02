eb1337da926d2bcca0e7b7f4487688c8357e71f6
andrepaixao <andrepaixaao@gmail.com>
2020-04-02 21:18:00 +0100

    Possivel interface principal terminada

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,102 @@
+$(document).ready(function(){
+var availableTarefas = [
+    "Andar",
+    "Correr",
+    "Tempo em pé",
+    "Pegar no rato",
+    "Movimentar o rato",
+    "Concentração e foco no monitor"
+  ];
+
+var availableSintomas= [
+    "Dor nos Dedos","Dores nos Membros","Movimentação reduzida","Fadiga Muscular","Formigueiro","Inflamação",
+    "Sensibilidade Alterada","Stress","Dores de cabeça"
+
+];
+  function split( val ) {
+    return val.split( /,\s*/ );
+  }
+  function extractLast( term ) {
+    return split( term ).pop();
+  }
+  $( "#tarefas" )
+    // don't navigate away from the field on tab when selecting an item
+    .on( "keydown", function( event ) {
+      if ( event.keyCode === $.ui.keyCode.TAB &&
+          $( this ).autocomplete( "instance" ).menu.active ) {
+        event.preventDefault();
+      }
+    })
+    .autocomplete({
+      minLength: 0,
+      source: function( request, response ) {
+        // delegate back to autocomplete, but extract the last term
+        response( $.ui.autocomplete.filter(
+          availableTarefas, extractLast( request.term ) ) );
+      },
+      focus: function() {
+        // prevent value inserted on focus
+        return false;
+      },
+      select: function( event, ui ) {
+        var terms = split( this.value );
+        // remove the current input
+        terms.pop();
+        // add the selected item
+        terms.push( ui.item.value );
+        // add placeholder to get the comma-and-space at the end
+        terms.push( "" );
+        this.value = terms.join( ", " );
+        return false;
+      }
+    });
+    $( "#sintomas" )
+    // don't navigate away from the field on tab when selecting an item
+    .on( "keydown", function( event ) {
+      if ( event.keyCode === $.ui.keyCode.TAB &&
+          $( this ).autocomplete( "instance" ).menu.active ) {
+        event.preventDefault();
+      }
+    })
+    .autocomplete({
+      minLength: 0,
+      source: function( request, response ) {
+        // delegate back to autocomplete, but extract the last term
+        response( $.ui.autocomplete.filter(
+          availableSintomas, extractLast( request.term ) ) );
+      },
+      focus: function() {
+        // prevent value inserted on focus
+        return false;
+      },
+      select: function( event, ui ) {
+        var terms = split( this.value );
+        // remove the current input
+        terms.pop();
+        // add the selected item
+        terms.push( ui.item.value );
+        // add placeholder to get the comma-and-space at the end
+        terms.push( "" );
+        this.value = terms.join( ", " );
+        return false;
+      }
+    });
+
+})
 function determinarPOP()
 {
     var x = document.getElementById("POP");
     x.style.display="block";
+}
+
+function addLinha()
+{
+    console.log("teste");
+    var x = document.getElementById("patEscolhidas")
+    x.innerHTML+="<tr><td></td><td></td><td></td></tr>";
+}
+
+function verResultados()
+{
+  window.location.href="prescricao.html";
 }
