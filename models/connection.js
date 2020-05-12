a04fc8cd650bc9a9aad4ca553f0de2cbc7ef0e28
andrepaixao <andrepaixaao@gmail.com>
2020-05-12 11:11:53 +0100

    alteracoes pequenas de interface

--- models/connection.js
+++ models/connection.js
@@ -1,11 +1,13 @@
 var mysql = require('mysql');
 
 var pool  = mysql.createPool({
+    connectionLimit:30,
     host     : 'remotemysql.com',
     user     : 'YfKYvyztwT',
     password : 'Z0zxhuNmSy',
     database : 'YfKYvyztwT',
-    port:3306
+    port:3306,
+    multipleStatements:true
 });
 
 module.exports.pool = pool;
